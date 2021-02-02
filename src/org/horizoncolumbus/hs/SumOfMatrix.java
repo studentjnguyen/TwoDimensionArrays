23ec81e3edcf18ba13436752998720353794126f
Justin <student.j.nguyen@horizoncolumbus.org>
2021-02-02 09:21:55 -0500

    classwork for tuesday-wednesday

--- src/org/horizoncolumbus/hs/SumOfMatrix.java
+++ src/org/horizoncolumbus/hs/SumOfMatrix.java
@@ -8,9 +8,18 @@ public class SumOfMatrix {
 
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 1; j++){
+                System.out.print("Row " + (i+1) + ": ");
                 System.out.print(matrix[i][j] + matrix[i][j+1] + matrix[i][j+2]);
             }
             System.out.println();
+        }
+
+        for(int i = 0; i < 1; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print("Column " + (j+1) + ": ");
+                System.out.print(matrix[i][j] + matrix[i+1][j] + matrix[i+2][j] + matrix[i+3][j]);
+                System.out.println();
+            }
         }
 
     }
