994a18e0d36d8793b5a410e5e00591813472ae68
Justin <student.j.nguyen@horizoncolumbus.org>
2021-02-04 10:31:13 -0500

    changed to columns being first

--- src/org/horizoncolumbus/hs/ArrayOnTheFly.java
+++ src/org/horizoncolumbus/hs/ArrayOnTheFly.java
@@ -7,23 +7,23 @@ public class ArrayOnTheFly {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter Column size: ");
-        int columnSize = input.nextInt();
         System.out.print("Please enter Row size: ");
         int rowSize = input.nextInt();
-        int[][] matrix = new int[columnSize][rowSize];
+        System.out.print("Please enter Column size: ");
+        int columnSize = input.nextInt();
+        int[][] matrix = new int[rowSize][columnSize];
 
-        for(int i = 0; i < columnSize; i++) {
-            for (int j = 0; j < rowSize; j++) {
-                System.out.print("Enter Column " + i + " Row " + j + " Size: ");
+        for(int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < columnSize; j++) {
+                System.out.print("Enter Row " + i + " Column " + j + " Size: ");
                 matrix[i][j] = input.nextInt();
             }
         }
 
         System.out.println();
 
-        for(int i = 0; i < columnSize; i++){
-            for(int j = 0; j < rowSize; j++){
+        for(int i = 0; i < rowSize; i++){
+            for(int j = 0; j < columnSize; j++){
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
